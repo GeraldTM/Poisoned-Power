dc5e49bd0ff264917e07dee785bcdd86bb2fca41
GeraldTM <99144289+GeraldTM@users.noreply.github.com>
2024-07-31 08:51:45 -0400

    credits fix

--- assets/scenes/Credits.gd
+++ assets/scenes/Credits.gd
@@ -10,9 +10,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if credits_toggled:
-		%Label.show
+		%Label.show()
 	else:
-		%Label.hide
+		%Label.hide()
 
 func _on_pressed():
 	credits_toggled = !credits_toggled
